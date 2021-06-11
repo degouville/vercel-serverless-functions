ab9d4c4a5257a166239fcf16ffe1371e3396d5e2
Mathieu de Gouville <mathieu@degouville.com>
2021-06-11 14:38:34 -0400

    Add trim option

--- api/slugify/[text].js
+++ api/slugify/[text].js
@@ -1,9 +1,17 @@
 import slugify from 'slugify'
 
 const options = { lower: true }
+const trimText = (text, from, to) => 
+  text
+    .split(from).pop()
+    .split(to).shift()
 
-module.exports = ({ query: { text } }, res) => {
-  const slug = slugify(text, options)
+module.exports = ({ query: { text: t, ...params } }, res) => {
+  const { trimFrom: from, trimTo: to } = params
 
-  res.json({ slug })
+  const isTrimable = !!from && !!to
+  const text = isTrimable ? trimText(t, from, to) : t
+  const slug = slugify( text, options )
+
+  res.json({ slug, params })
 }
