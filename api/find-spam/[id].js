bf825d3e0766a298ff4865d6985e984dbc839621
Mathieu de Gouville <mathieu@degouville.com>
2021-09-07 15:53:13 -0400

    create findSpam func

---
+++ api/find-spam/[id].js
@@ -0,0 +1,12 @@
+const has = b => a => b.includes(a)
+const findOccurance = (a, b) => !!a
+  .toLowerCase()
+  .replace('-', ' ')
+  .split(' ')
+  .find(has(b))
+
+// module.exports = ({ query: { params: { mail, name } } }, res) => {
+module.exports = ({ query: { id, mail, name } }, res) => {
+  const isSuspect = !findOccurance(name, mail)
+  res.json({ id, mail, name, isSuspect })
+}
