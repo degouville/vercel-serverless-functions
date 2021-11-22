34024ecc28239734fee0b5f5b5410924f4e1fb0a
Mathieu de Gouville <mathieu@degouville.com>
2021-11-22 13:23:49 -0300

    FIX loop

--- api/find-spam/[id].js
+++ api/find-spam/[id].js
@@ -1,11 +1,14 @@
+// const has = b => a => b.includes(a)
 const has = b => a => b.includes(a)
+const nonEmpty = e => e !== ''
+
 const findOccurance = (a, b) => !!a
   .toLowerCase()
   .replace('-', ' ')
   .split(' ')
+  .filter(nonEmpty)
   .find(has(b))
 
-// module.exports = ({ query: { params: { mail, name } } }, res) => {
 module.exports = ({ query: { id, mail, name } }, res) => {
   const isSuspect = !findOccurance(name, mail)
   res.json({ id, mail, name, isSuspect })
